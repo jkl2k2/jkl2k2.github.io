51d05a2a05979459fe6f8500309b321d396e54bc
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-03-22 18:40:28 -0700

    Remove image test header, added padding to image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,7 @@ export default function Home() {
         <a href="/"><b>Home</b></a>
         <a href="/about/"><b>About</b></a>
       </div>
-      <div class="customHeader">
-        <p><h2>Image test</h2></p>
-      </div>
-      <div>
+      <div style={{ padding: 30 }}>
         <img src="https://cdn.discordapp.com/attachments/785080637144956938/823707286156476477/alien.png" alt="" />
       </div>
     </div>
