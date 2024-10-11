8d31c34fcf99b059d730e08b1a4c569a1685bc2e
Josh <joshualomelin@gmail.com>
2024-10-11 13:17:58 -0700

    Fix for undefined document error on build

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import Nav from './Nav.js';
 import Home from './Home.js';
 import Projects from './Projects.js';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HashRouter>
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
+            </HashRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/app/[[...slug]]/client.js
+++ src/app/[[...slug]]/client.js
@@ -2,14 +2,11 @@
 
 import React from 'react';
 import dynamic from 'next/dynamic';
-import { HashRouter } from 'react-router-dom';
 
 const App = dynamic(() => import('../../App'), { ssr: false })
 
 export function ClientOnly() {
     return (
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <App />
     )
 }
